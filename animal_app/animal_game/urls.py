9f718839ed901e70c7a826855d6d2d367ef4acc9
Pierre Hébert <cpierrot273@gmail.com>
2021-01-24 11:57:35 -0700

    add loadcsv to deployment process

--- animal_app/animal_game/urls.py
+++ animal_app/animal_game/urls.py
@@ -17,8 +17,9 @@ from django.contrib import admin
 from django.urls import path, include
 from play_game import views
 from django.conf.urls.static import static
+from . import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('play_game.urls')),
-] # + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
