667d775d96510670f0b2111911bc6ca537e4c6c3
Pierre Hébert <cpierrot273@gmail.com>
2021-01-24 10:39:13 -0700

    run terraform fmt

--- deployment/demo.tf
+++ deployment/demo.tf
@@ -11,26 +11,26 @@ provider "openstack" {
 }
 
 resource "openstack_compute_secgroup_v2" "public" {
-  name = "public_secgroup"
+  name        = "public_secgroup"
   description = "django only security group"
   rule {
-    from_port = 8000
-    to_port = 8000
+    from_port   = 8000
+    to_port     = 8000
     ip_protocol = "tcp"
-    cidr = "0.0.0.0/0"
+    cidr        = "0.0.0.0/0"
   }
   rule {
-    from_port = 8000
-    to_port = 8000
+    from_port   = 8000
+    to_port     = 8000
     ip_protocol = "tcp"
-    cidr = "::/0"
+    cidr        = "::/0"
   }
 }
 
 resource "openstack_compute_instance_v2" "main_instance" {
-  name = "main_instance"
-  image_name = "django-image"
-  flavor_name = "m1.small"
-  security_groups = [ openstack_compute_secgroup_v2.public.name ]
-  key_pair = "KP1"
+  name            = "main_instance"
+  image_name      = "django-image"
+  flavor_name     = "m1.small"
+  security_groups = [openstack_compute_secgroup_v2.public.name]
+  key_pair        = "KP1"
 }
